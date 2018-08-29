8b83057a455ce6371f2c135d0feeec0cf50932af
leo <firelion0725@163.com>
2018-08-29 15:46:38 +0800

    增加BaseActivity

--- app/src/main/java/com/leo/test/base/BaseActivity.java
+++ app/src/main/java/com/leo/test/base/BaseActivity.java
@@ -1,6 +1,13 @@
 package com.leo.test.base;
 
+import android.os.Bundle;
+
+import java.lang.reflect.ParameterizedType;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 /**
  * @author leo, ZhangWei
@@ -9,4 +16,26 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public abstract class BaseActivity<T extends BaseViewModel> extends AppCompatActivity {
     protected T viewModel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setBaseViewModel(getTClass());
+    }
+
+    private void setBaseViewModel(@NonNull Class<T> modelClass) {
+        viewModel = ViewModelProviders.of(this).get(modelClass);
+        getLifecycle().addObserver(viewModel);
+    }
+
+    private Class<T> getTClass() {
+        return (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    @Override
+    protected void onDestroy() {
+        viewModel.onCleared();
+        viewModel = null;
+        super.onDestroy();
+    }
 }
---
+++ app/src/main/java/com/leo/test/ui/main/TestActivity.java
@@ -0,0 +1,26 @@
+package com.leo.test.ui.main;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.leo.test.R;
+import com.leo.test.base.BaseActivity;
+
+public class TestActivity extends BaseActivity<MainViewModel> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        viewModel.data.observe(this, taskModel -> {
+            if (taskModel != null) {
+                Log.i("observe", "" + taskModel);
+            }else{
+                Log.i("observe", "no data");
+            }
+        });
+
+        viewModel.getData();
+    }
+}
